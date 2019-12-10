00df19ea096716b262ad7c430d3c29342defbea7
Nicolas Forero <nicolasforero1199@gmail.com>
2019-12-09 20:09:22 -0500

    Fixed content of the file holberton

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,10 @@
 
 $owner = 'www-data'
 file { 'holberton':
+  ensure  => present,
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => $owner,
   group   => $owner,
-  content => 'I Love Puppet',
+  content => 'I love Puppet',
 }
