c70aeafc92f7ab74c408406e59adca4c1d1a1d7d
NickForero11 <nicolasforero1199@gmail.com>
2020-01-06 02:26:07 -0500

    Added Puppet manifest to configure a simple Nginx server as script 0

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,33 @@
+# Manifest that install Nginx and configures:
+#   a simple default page.
+#   a custom header with the server hostname.
+
+package { 'Install Nginx':
+  ensure => installed,
+  name   => 'nginx'
+}
+-> exec { 'Allow Nginx http over the firewall':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/sbin/'
+}
+
+-> file_line {'Add default page':
+  ensure             => 'present',
+  path               => '/var/www/html/index.nginx-debian.html',
+  line               => 'Holberton School',
+  append_on_no_match => true
+}
+
+-> file_line {'Add custom header':
+  ensure             => 'present',
+  path               => '/etc/nginx/nginx.conf',
+  line               => '       add_header X-Served-By $hostname;',
+  append_on_no_match => true,
+  after              => 'sendfile on;'
+}
+
+-> service { 'Check Nginx service':
+  ensure     => running,
+  enable     => true,
+  name       => 'nginx'
+}
