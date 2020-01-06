63ef92aeb3b68245ec944dd3185ce411bbf28cb7
NickForero11 <nicolasforero1199@gmail.com>
2020-01-06 03:27:27 -0500

    Fixed style

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,36 +2,26 @@
 #   a simple default page.
 #   a custom header with the server hostname.
 
-package { 'Install Nginx':
-  ensure => installed,
-  name   => 'nginx'
-}
-
-file {'Add default page':
-  path    => '/var/www/html/index.html',
+file { '/var/www/html/index.html':
   content => 'Holberton School',
-  require => Package['Install Nginx']
 }
 
-file_line {'Add custom header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'sendfile on;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['Install Nginx']
+file_line { 'writes redirection site':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file_line { 'Add redirectme':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
-  require => Package['Install Nginx']
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;',
 }
 
-service { 'Check Nginx service':
+service { 'nginx':
   ensure  => 'running',
   enable  => true,
-  name    => 'nginx',
-  require => Package['Install Nginx']
+  require => Package['nginx'],
 }
