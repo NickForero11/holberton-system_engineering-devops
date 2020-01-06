62355107bc1777d90f0d5425be974ba08aa12478
NickForero11 <nicolasforero1199@gmail.com>
2020-01-06 03:09:05 -0500

    Added redirection to manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -22,6 +22,13 @@ package { 'Install Nginx':
   append_on_no_match => true
 }
 
+-> file_line { 'Add redirectme':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
+
 service { 'Check Nginx service':
   ensure  => 'running',
   enable  => true,
