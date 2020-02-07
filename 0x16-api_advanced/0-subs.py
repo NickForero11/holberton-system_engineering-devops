1be2caddcd8d77b327706408977abd4a3ddb5f4b
NickForero11 <nicolasforero1199@gmail.com>
2020-02-06 20:08:50 -0500

    Added the project 0x16-api_advanced with a Python function to check the subscribers of a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that contains a handlers to interact with the Reddit API.
+"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """Function to check the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+
+
+    Arguments:
+        subreddit (str) - The name of the subreddit that will be processed.
+
+    Returns:
+        int:    The number of total subscribers of the given subreddit,
+                If an invalid subreddit is given, return 0.
+    """
+    URL = 'https://www.reddit.com/r/{}/about.json'
+    data = get(
+        URL.format(subreddit),
+        allow_redirects=False,
+        headers={'User-Agent': 'fake_user_agent'})
+    if data.status_code != 200:
+        return 0
+    json_data = data.json()
+    parsed_data = json_data.get('data')
+    subscribers = parsed_data.get('subscribers')
+    return subscribers
